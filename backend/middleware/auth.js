92d5c64578adb92985475c5f4d81325b34942976
kyungho0511 <kleearchitecture@gmail.com>
2025-03-09 22:26:30 -0400

    Add configuration

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import * as userRepository from "../data/auth.js";
+import { config } from "../config.js";
 
 const AUTH_ERROR = { message: "Authentication Error" };
 
@@ -11,22 +12,18 @@ export const isAuth = async (req, res, next) => {
   }
   const token = authHeader.split(" ")[1];
 
-  jwt.verify(
-    token,
-    "qI7MAa7Z@L2Ql-,AxnlT>[M1,&2braW",
-    async (error, decoded) => {
-      // JWT verification
-      if (error) {
-        return res.status(401).json(AUTH_ERROR);
-      }
-      // Check if the user exists in the database
-      const user = await userRepository.findById(decoded.id);
-      if (!user) {
-        return res.status(401).json(AUTH_ERROR);
-      }
-      // 앞으로의 미들웨어에서 공통적을 접근 필요시, req.customData 로 등록 해줄 수 있다.
-      req.userId = user.id;
-      next();
+  jwt.verify(token, config.jwt.secretKey, async (error, decoded) => {
+    // JWT verification
+    if (error) {
+      return res.status(401).json(AUTH_ERROR);
     }
-  );
+    // Check if the user exists in the database
+    const user = await userRepository.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json(AUTH_ERROR);
+    }
+    // 앞으로의 미들웨어에서 공통적을 접근 필요시, req.customData 로 등록 해줄 수 있다.
+    req.userId = user.id;
+    next();
+  });
 };
